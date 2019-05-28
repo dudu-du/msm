f4c754e28d0c1ba74d3184af531b88f1e78cdab8
wuzhenzhen <418376499@qq.com>
2019-05-28 22:17:45 +0800

    增加接口：月检查统计

--- service/src/main/java/com/safety/service/ICheckMonthRecordListService.java
+++ service/src/main/java/com/safety/service/ICheckMonthRecordListService.java
@@ -2,6 +2,11 @@ package com.safety.service;
 
 import com.safety.entity.CheckMonthRecordList;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.safety.entity.CheckRectificationReceipt;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +17,52 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-05-23
  */
 public interface ICheckMonthRecordListService extends IService<CheckMonthRecordList> {
+    /**
+     * 统计月检查表中否的清单个数
+     * @param orgId
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws Exception
+     */
+    List<Map<String,Object>> getChecklistResultCountByOrg(String orgId, LocalDate startTime, LocalDate endTime) throws Exception;
 
+    /**
+     * 统计月治理检查表中否的清单对应的等级个数
+     * @param orgId
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws Exception
+     */
+    List<Map<String,Object>> getChecklistLevelCountByOrg(String orgId, LocalDate startTime, LocalDate endTime) throws Exception;
+    /**
+     * 统计月治理检查表中否的台账个数
+     * @param orgId
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws Exception
+     */
+    List<Map<String,Object>> getLedgerResultCountByOrg(String orgId, LocalDate startTime, LocalDate endTime) throws Exception;
+
+    /**
+     * 统计月治理检查表中否的清单对应的台账个数
+     * @param orgId
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws Exception
+     */
+    List<Map<String,Object>> getLedgerLevelCountByOrg(String orgId, LocalDate startTime, LocalDate endTime) throws Exception;
+
+    /**
+     * 获取月治理检查表中否的回执单列表
+     * @param orgId
+     * @param startTime
+     * @param endTime
+     * @return
+     * @throws Exception
+     */
+    List<CheckRectificationReceipt> getReceiptListByOrg(String orgId, LocalDate startTime, LocalDate endTime) throws Exception;
 }
