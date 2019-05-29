9802f190e96fa92549f5b33b886f3413d652b024
qinzhihui <851952906@qq.com>
2019-05-29 11:42:29 +0800

    增加回执单列表接口

--- service/src/main/java/com/safety/service/impl/CheckRectificationReceiptServiceImpl.java
+++ service/src/main/java/com/safety/service/impl/CheckRectificationReceiptServiceImpl.java
@@ -1,10 +1,15 @@
 package com.safety.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.safety.entity.CheckRectificationReceipt;
 import com.safety.mapper.CheckRectificationReceiptMapper;
 import com.safety.service.ICheckRectificationReceiptService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +22,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CheckRectificationReceiptServiceImpl extends ServiceImpl<CheckRectificationReceiptMapper, CheckRectificationReceipt> implements ICheckRectificationReceiptService {
 
+    @Autowired
+    private CheckRectificationReceiptMapper checkRectificationReceiptMapper;
+
+    @Override
+    public PageInfo<CheckRectificationReceipt> getByPage(Integer currentPage, Integer pageSize) {
+        PageHelper.startPage(currentPage, pageSize);
+        List<CheckRectificationReceipt> checkMonthRecords = checkRectificationReceiptMapper.selectAll();
+        return new PageInfo<>(checkMonthRecords);
+    }
 }
